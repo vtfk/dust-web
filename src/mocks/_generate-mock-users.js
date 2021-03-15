b1e57245dcbf976684cd88343d4ffe55f4cd8a6a
Mats Andreassen <matsand@live.no>
2021-03-15 17:44:51 +0100

    La til metode for generering av users

---
+++ src/mocks/_generate-mock-users.js
@@ -0,0 +1,56 @@
+const { writeFileSync } = require('fs')
+const { ansattDep, elevDep } = require('./_departments')
+const fregansatte = require('./_tenor-eksport-ansatt.json')
+const fregelever = require('./_tenor-eksport-elev.json')
+
+const shuffle = array => array.sort(() => 0.5 - Math.random())
+const getRandom = (min, max) => Math.floor(Math.random() * max) + min
+const titleCase = str => str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ')
+const replaceIllegalChars = (str, joinchar) => str.toLowerCase().replace(/ /g, (joinchar || '')).replace(/æ/g, 'ae').replace(/ø/g, 'o').replace(/å/g, 'aa')
+
+const randomId = () => {
+  const timestamp = (new Date().getTime() / 1000 | 0).toString(16)
+  return timestamp + 'xxxxxxxxxxxxxxxx'.replace(/[x]/g, () => {
+    return (Math.random() * 16 | 0).toString(16)
+  }).toLowerCase()
+}
+
+const persons = []
+const getUser = (fregPerson, employee = false) => {
+  // Generate username
+  const usernameStart = `${replaceIllegalChars(fregPerson.fornavn).substring(0, 3)}${fregPerson.id.substring(0, 4)}`
+  const usernameMatch = persons.filter(per => per.samAccountName.startsWith(usernameStart)).length
+  const username = `${usernameStart}${usernameMatch > 0 ? usernameMatch : ''}`
+
+  // Generate email address
+  const mail = replaceIllegalChars(`${employee ? fregPerson.visningnavn : username}@${employee ? 'vtfk.no' : 'skole.vtfk.no'}`, '.')
+
+  const department = employee ? shuffle(ansattDep)[0] : shuffle(elevDep)[0]
+  const pers = {
+    id: randomId(),
+    samAccountName: username,
+    displayName: titleCase(fregPerson.visningnavn),
+    givenName: titleCase(fregPerson.fornavn),
+    surName: titleCase(fregPerson.etternavn),
+    mail,
+    userPrincipalName: mail,
+    proxyAddresses: [`SMTP:${mail}`],
+    domain: employee ? 'login' : 'skole',
+    employeeNumber: fregPerson.id,
+    timestamp: new Date().toISOString(),
+    enabled: getRandom(0, 100) !== 42,
+    ou: getRandom(0, 100) === 42 ? 'AUTO DISABLED USERS' : 'AUTO USERS',
+    departmentShort: employee ? department.extensionAttribute6 : department.department,
+    departmentAdditional: null,
+    office: employee ? department.physicalDeliveryOfficeName : department.company
+  }
+
+  persons.push(pers)
+  return pers
+}
+
+const users = []
+fregansatte.forEach(ansatt => users.push(getUser(ansatt, true)))
+fregelever.forEach(elev => users.push(getUser(elev)))
+
+writeFileSync('./mock-users.json', JSON.stringify(users, null, 2))
